ef9688185c19d57821e1f11870581959e3dd9e51
esionascimento <esio.392@protonmail.com>
2021-10-20 09:06:32 -0400

    conhecendo o booleano e if

---
+++ aula3-operadores-logicos.py
@@ -0,0 +1,12 @@
+verdade = True
+falso = False
+
+a = 50
+b = 20
+
+print(type(verdade))
+
+if verdade == True:
+  print('Verdade é verdadeiro')
+if ((a>b) and ('joao' == 'maria') or True):
+  print(a, 'é maior do que', b)
