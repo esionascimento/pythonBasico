6065b42b39cd49c399f05737661730990dddb8a9
esionascimento <esio.392@protonmail.com>
2021-10-20 08:48:36 -0400

    calculator

--- aula2-entrada-e-saida.py
+++ aula2-entrada-e-saida.py
@@ -1,16 +1,6 @@
-print('Hello World!\n')
-print('segundo print')
+numero1 = 27
+numero2 = 53
 
-nome = input('Escreva seu nome: ')
-idade = input('Escreva sua idade: ')
-altura = input('Escreva sua altura: ')
-tipo_idade = type(idade)
-tipo_altura = type(altura)
+resultado = numero1 + numero2
 
-print(nome)
-print(type(nome))
-print(tipo_idade)
-print(tipo_altura)
-frase = nome, 'tem', idade, 'anos e', altura, 'de altura'
-print(nome + ' tem ' + str(idade) + ' anos e ' + str(altura) + ' de altura')
-print(frase)
+print(resultado)
