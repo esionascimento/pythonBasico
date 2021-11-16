414717e1301927bfd62b89312215aa3e19116de4
esionascimento <esio.392@protonmail.com>
2021-11-16 00:53:38 -0400

    create class / usando a class

---
+++ oo-aula9-orientacao-a-objeto/main.py
@@ -0,0 +1,22 @@
+from veiculo import Veiculo
+
+veiculo1 = Veiculo('preto', 6, 'ford', 30)
+
+print(veiculo1)
+print(type(veiculo1))
+
+print("Carro 1")
+print("Cor:", veiculo1.cor)
+print("Rodas:", veiculo1.rodas)
+print("Marca:", veiculo1.marca)
+
+print("")
+veiculo2 = Veiculo('branco', 4, 'bmw', 50)
+
+print("Carro 2")
+print("Cor:", veiculo2.cor)
+print("Rodas:", veiculo2.rodas)
+print("Marca:", veiculo2.marca)
+print("Tanque", veiculo2.tanque)
+veiculo2.abastecer(35)
+print("Tanque", veiculo2.tanque)
