cf4fdde687eb4aaba83379799a4fe5ca61b1d9ab
esionascimento <esio.392@protonmail.com>
2021-11-17 00:59:56 -0400

    tentando abrir um arquivo inexistente

--- aula11-tratamento-de-erros.py
+++ aula11-tratamento-de-erros.py
@@ -1,3 +1,5 @@
+import time
+
 try:
   a = 1200 / 0
 except ZeroDivisionError: #erro expecifico
@@ -8,3 +10,17 @@ except Exception as erro: #salva o erro na variavel para usar posteriormente
   print("Qualquer exceção", erro)
 
 print("Hello")
+
+def abre_arquivo():
+  try:
+    abre_arquivo = open('arquivo1.txt')
+    return True
+  except Exception as erro:
+    print("Erro:", erro)
+    return False
+
+while not abre_arquivo():
+  print("Tentando abrir o arquivo")
+  time.sleep(5)
+
+print("Consegui abrir o arquivo")
