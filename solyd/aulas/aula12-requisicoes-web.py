b1006350667c3efea174d9f1bc308b96c35663ce
esionascimento <esio.392@protonmail.com>
2021-11-21 15:50:05 -0400

    requests web g1.com.br usando try catch

--- solyd/aulas/aula12-requisicoes-web.py
+++ solyd/aulas/aula12-requisicoes-web.py
@@ -4,3 +4,12 @@ sudo apt-get install python3-pip # para instalar o pip3
 sudo rm /usr/bin/pip # caso você tenha o pip já instalado
 sudo mv /usr/bin/pip3 /usr/bin/pip # apenas para renomear o arquivo e tornálo mais acessivél .....
 sudo pip install --upgrade pip # atualizar para a última versão do gerenciador pip """
+import requests
+
+try:
+  requisicao = requests.get('https://g1.com.br')
+  print(requisicao)
+  print(requisicao.status_code)
+  print(requisicao.text)
+except Exception as e:
+  print("Requisicao deu erro:", e)
