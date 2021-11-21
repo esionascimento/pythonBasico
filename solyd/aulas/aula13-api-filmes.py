72ced0da7e9cb88e725f011b8e5fe06c87ecce2b
esionascimento <esio.392@protonmail.com>
2021-11-21 17:25:29 -0400

    create function para passar o filme que o usuario pretende buscar

--- solyd/aulas/aula13-api-filmes.py
+++ solyd/aulas/aula13-api-filmes.py
@@ -1,20 +1,35 @@
 import requests
 import json # trabalha com json, tranforma json em objetos
 
-req = None
-site = 'https://www.omdbapi.com/?apikey=baf71168&t=interstellar'
+site = 'https://www.omdbapi.com/?apikey=baf71168&t='
 
-try:
-  req = requests.get(site)
-except:
-  print('Erro na conexão')
-  exit()
+def requisicao(titulo):
+  try:
+    req = requests.get(site + titulo)
+    dicionario = json.loads(req.text) # converte json em dicionario
+    return dicionario
+  except:
+    print('Erro na conexão')
+    return None
 
-dicionario = json.loads(req.text) # converte json em dicionario
+def printar_detalhes(film):
+  print('===Filme===')
+  print('Título:', film['Title'])
+  print('Ano:', film['Year'])
+  print('Diretor:', film['Director'])
+  print('Atores:', film['Actors'])
+  print('Nota:', film['imdbRating'])
+  print('')
 
-print(dicionario, '\n')
-print('Título:', dicionario['Title'])
-print('Ano:', dicionario['Year'])
-print('Diretor:', dicionario['Director'])
-print('Atores:', dicionario['Actors'])
-print('Nota:', dicionario['imdbRating'])
+sair = False
+while not sair:
+  op = input('Escreva o nome de um filme ou SAIR para fechar: ')
+
+  if op == 'SAIR':
+    sair = True
+  else:
+    film = requisicao(op)
+    if film['Response'] == 'False':
+      print('Filme não encontrado')
+    else:
+      printar_detalhes(film)
