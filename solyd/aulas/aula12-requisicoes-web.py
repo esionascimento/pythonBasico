3182c86052499e76442293647a6a2b586b6f6793
esionascimento <esio.392@protonmail.com>
2021-11-21 15:36:57 -0400

    passos para instalar pip3 em sistemas UNIX

---
+++ solyd/aulas/aula12-requisicoes-web.py
@@ -0,0 +1,6 @@
+""" Para sistemas UNIX*
+
+sudo apt-get install python3-pip # para instalar o pip3
+sudo rm /usr/bin/pip # caso você tenha o pip já instalado
+sudo mv /usr/bin/pip3 /usr/bin/pip # apenas para renomear o arquivo e tornálo mais acessivél .....
+sudo pip install --upgrade pip # atualizar para a última versão do gerenciador pip """
